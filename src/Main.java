cfe0bed176c9f2b0bf1810a3b24e1f1ee61194e5
karenJCastillo <kc6539769@gmail.com>
2025-02-18 22:36:58 -0500

    cambiosK

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Scanner entrada =new Scanner(System.in);
+        String nombre ;
+        int edad ;
+        String hobby ;
+        System.out.println("escribe tu nombre");
+        nombre = entrada.next();
+        System.out.println("escribe tu edad");
+        edad= entrada.nextInt();
+        System.out.println("¿Cual es tu hobby?");
+        hobby = entrada.next();
+        System.out.println("tu nombre es  " + nombre );
+        System.out.println("tu edad es de  "+ edad+"   "+"años");
+        if (edad <= 17)  {
+            System.out.println("eres menor de edad ");
+        } else {
+            System.out.println("eres mayor de edad ");
+        }
+        System.out.println("tu hobby es "+ hobby );
+    }
+}
