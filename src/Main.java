7446898f36aacb116da942d5784cc998dcbfa7d3
Manuelrkocortes <meduardocortes@ucundinamarca.edu.co>
2025-02-21 16:34:27 -0500

    c

--- src/Main.java
+++ src/Main.java
@@ -25,5 +25,7 @@ public class Main {
         System.out.println("algo que queremos ppp ");
         System.out.println("cambioooo uno");
         System.out.println("cambio dos");
+        System.out.println("cambio tres");
+        System.out.println("cambbbb");
     }
 }
