450af1afcb764e375e8911fec32086db0df33e0a
Karen Castillo <kc6539769@gmail.com>
2025-02-20 20:06:01 -0500

    cambiouno

--- src/Main.java
+++ src/Main.java
@@ -22,7 +22,7 @@ public class Main {
             System.out.println("eres mayor de edad ");
         }
         System.out.println("tu hobby es "+ hobby );
-        System.out.println("algo que queremos ");
-        System.out.println("algo que no queremos");
+        System.out.println("algo que queremos ppp ");
+        System.out.println("cambioooo uno");
     }
 }
