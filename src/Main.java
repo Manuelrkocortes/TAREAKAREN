975aa6f62816ba298284896f20b400b5b5904c9e
Manuelrkocortes <meduardocortes@ucundinamarca.edu.co>
2025-02-21 16:36:17 -0500

    1

--- src/Main.java
+++ src/Main.java
@@ -27,5 +27,6 @@ public class Main {
         System.out.println("cambio dos");
         System.out.println("cambio tres");
         System.out.println("cambbbb");
+        System.out.println("ggg");
     }
 }
